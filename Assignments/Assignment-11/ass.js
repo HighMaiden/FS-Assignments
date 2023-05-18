1245ad4c84b76c6e492cdae5a78b539b1acd588a
Ali Ozkan <aliozkan4334@gmail.com>
2023-05-18 16:23:40 +0200

    made a reverseWord function

--- Assignments/Assignment-11/ass.js
+++ Assignments/Assignment-11/ass.js
@@ -4,3 +4,9 @@ function reverseString(sentence){
     return sentence.split("").reverse().join("");
 }
 console.log(reverseString("hello world"));
+
+
+function reverseWord(x){
+    return x.split(" ").reverse().join(" ") 
+}
+console.log(reverseWord("? doing you are how hi"));
