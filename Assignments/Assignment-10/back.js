a99e386fa420cc59bfc31df4192f6b7d9b003abc
Ali Ozkan <aliozkan4334@gmail.com>
2023-05-18 16:11:29 +0200

    opened new js and html for assignment

---
+++ Assignments/Assignment-10/back.js
@@ -0,0 +1,20 @@
+// soru-4 sizden 1'den 100'e kadar sayıları konsola yazdırmanız istenmektedir ancak 3'ün katlarına gelince 'Fizz', 5'in katlarına glince 'Buzz' yazacak. Hem 'ün hemde 5'in katına geldiğinde ise 'FizzBuzz' yazdırmanız istenmektedir.
+
+
+
+// for(let i=1;i<100;i++){
+//     if(i%3==0 && i%5==0)
+//     console.log(`${i}FizzBuzz`);
+//     else if(i%5==0){
+//         console.log(`${i}Buzz`);
+//     }
+//     else if(i%3 == 0){
+//         console.log(`${i}Fizz`);
+//     }
+//   else{
+//     console.log(`${i}`);
+//   }
+// }
+
+// const num = Math.floor(Math.random()*100)+1
+// for(let i =)
