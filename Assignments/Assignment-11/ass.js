f3c70cd407714607d1689a140a7447fb9f65b5c9
Ali Ozkan <aliozkan4334@gmail.com>
2023-05-19 21:24:57 +0200

    add a new div in img and changed the nav a bit

--- Assignments/Assignment-11/ass.js
+++ Assignments/Assignment-11/ass.js
@@ -7,6 +7,8 @@ console.log(reverseString("hello world"));
 
 
 function reverseWord(x){
+    // console.log(x.split(" "));
     return x.split(" ").reverse().join(" ") 
 }
 console.log(reverseWord("? doing you are how hi"));
+console.log(reverseWord("merhaba"));
