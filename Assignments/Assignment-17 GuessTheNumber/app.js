e0f65c3906fce7246e4a2d661a32b0dc0739e3e2
Ali Ozkan <aliozkan4334@gmail.com>
2023-05-26 11:12:05 +0200

    adding counter

--- Assignments/Assignment-17 GuessTheNumber/app.js
+++ Assignments/Assignment-17 GuessTheNumber/app.js
@@ -1,4 +1,26 @@
 const input1 =document.querySelector('.input')
 const btn1 =document.querySelector('.btn')
-const ınput2 =document.querySelector('.sonuc')
+const input2 =document.querySelector('.sonuc')
 const p =document.querySelector('p')
+const random = Math.floor(Math.random()*100)+1
+let counter =7
+// console.log(input1.value);
+
+btn1.addEventListener("click",()=>{
+if(counter>0){
+if(input1.value ==random){
+    sonuc.value =(`Kazandiniz`);
+}
+else if(input1.value > random){
+    sonuc.value =(`Daha kucuk bir sayi giriniz`);
+}
+else{
+sonuc.value=(` Daha buyuk bir sayi giriniz`);
+}
+
+    console.log(input1.value);
+}
+counter--}
+)
+
+p.innerHTML=`${counter} hakkiniz kaldi.`
