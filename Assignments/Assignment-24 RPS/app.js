adf640c73d2c9ba3bfb6c4f2011d7a565bd47213
Ali Ozkan <aliozkan4334@gmail.com>
2023-06-05 19:55:39 +0200

    create image

--- Assignments/Assignment-24 RPS/app.js
+++ Assignments/Assignment-24 RPS/app.js
@@ -1,9 +1,42 @@
+
+
+
+
+
 //* ------ Selectors ------- */
+//?RESIMLER
+const rockImg=document.getElementById("rock")
+const paperImg=document.getElementById("paper")
+const scissorImg=document.getElementById("scissor")
+//?Secilen Elemanlarin Tasiyicilari
+const yourChoiceDiv= document.getElementById("your-choice")
 
 //* ------- Variables ------- */
+let image = document.createElement("img")
+
+
+
 
 
 //* ------- Event Listeners ------- */
 
+rockImg.addEventListener("click",()=>{
+
+image.src="./assets/rock.png"
+image.alt="rock"
+yourChoiceDiv.appendChild(image)
+
+//?INNERHTML
+// yourChoiceDiv.innerHTML =`<img src="./assets/rock.png" alt="rock"  >`
+})
+
+
+
+
 
 //* ------- Functions ------- */
+
+
+
+
+
