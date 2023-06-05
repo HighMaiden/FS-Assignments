eb2d1daf157ce5ca80628c7b213f221584757fce
Ali Ozkan <aliozkan4334@gmail.com>
2023-06-05 21:26:07 +0200

    you win

--- Assignments/Assignment-24 RPS/app.js
+++ Assignments/Assignment-24 RPS/app.js
@@ -1,111 +1,83 @@
-
-
-
-
-
 //* ------ Selectors ------- */
-//?RESIMLER
-const rockImg=document.getElementById("rock")
-const paperImg=document.getElementById("paper")
-const scissorImg=document.getElementById("scissor")
-//?Secilen Elemanlarin Tasiyicilari
-const yourChoiceDiv= document.getElementById("your-choice")
-const pcChoiceDiv= document.getElementById("pc-choice")
 const selectionArticle = document.querySelector(".selection")
-//?message
-const messagePar =document.querySelector(".message")
+
+//? Secilen elemanlarin tayicilari
+const yourChoiceDiv = document.getElementById("your-choice")
+const pcChoiceDiv = document.getElementById("pc-choice")
+
+//? message
+const messagePar = document.querySelector(".message")
+
+//? Score
+const scoreCardSection = document.querySelector(".score-card")
+const pcScoreSpan = document.getElementById("pc-score")
+const yourScoreSpan = document.getElementById("your-score")
+
 //* ------- Variables ------- */
 let userSelectImg = document.createElement("img")
 let pcSelectImg = document.createElement("img")
-//?colors
-const Yellow= "#ffc538";
-const Red= "#fb778b";
-const Green="#5ab7ac";
-//?Score 
-
-const scoreCardSec = document.querySelector(".score-card")
-
-
-
+let pcRandom
+//? Colors
+const YELLOW = "#ffc538"
+const RED = "#fb778b"
+const GREEN = "#5ab7ac"
 
 //* ------- Event Listeners ------- */
-selectionArticle.addEventListener("click",(e)=>{
-    // console.log(e.target.id);
-    if(e.target.id){
-    //?can be done like this also
-    // if(
-    // e.target.id==="rock" ||
-    // e.target.id ==="scissor"||
-    // e.target.id === "paper"
-    // ){
-        userSelectImg.src =`./assets/${e.target.id}.png`
-        userSelectImg.alt = e.target.id
-        yourChoiceDiv.appendChild(userSelectImg)
-        createPcSelection()
-    }
+selectionArticle.addEventListener("click", (e) => {
+  // console.log(e.target.id)
+  if (e.target.id) {
+    userSelectImg.src = `./assets/${e.target.id}.png`
+    userSelectImg.alt = e.target.id
+    yourChoiceDiv.appendChild(userSelectImg)
+    createPcSelection()
+  }
 })
-
-
-
-
-//?ILKEL YONTEM
-// rockImg.addEventListener("click",()=>{
-
-// image.src="./assets/rock.png"
-// image.alt="rock"
-// yourChoiceDiv.appendChild(image)
-
-// //?INNERHTML
-// // yourChoiceDiv.innerHTML =`<img src="./assets/rock.png" alt="rock"  >`
-// })
-// paperImg.addEventListener("click",()=>{
-
-//     image.src="./assets/paper.png"
-//     image.alt="paper"
-//     yourChoiceDiv.appendChild(image)
-    
-
-//     })
-//     scissorImg.addEventListener("click",()=>{
-
-//         image.src="./assets/scissor.png"
-//         image.alt="scissor"
-//         yourChoiceDiv.appendChild(image)
-        
-    
-//         })
-    
-
-
-
-
 
 //* ------- Functions ------- */
 
-const createPcSelection = ()=>{
-    const pcArr=["rock","paper","scissor"]
-    const pcRandom=pcArr[Math.floor(Math.random()*3)]
-    pcSelectImg.src =`./assets/${pcRandom}.png`
-    pcSelectImg.alt = pcRandom
-    pcChoiceDiv.appendChild(pcSelectImg)
-    
-    calculateResult()
-    
-    }
-const draw = ()=>{
-    messagePar.textContent ="it's a draw"
-scoreCardSec.style.color="Yellow"
-messagePar.style.backgroundColor = "Yellow"
+const createPcSelection = () => {
+  const pcArr = ["rock", "paper", "scissor"]
+  pcRandom = pcArr[Math.floor(Math.random() * 3)]
+  pcSelectImg.src = `./assets/${pcRandom}.png`
+  pcSelectImg.alt = pcRandom
+  pcChoiceDiv.appendChild(pcSelectImg)
+  calculateResult()
 }
 
-    const calculateResult = ()=>{
-        // console.log(userSelectImg.alt);
-        // console.log(pcSelectImg.alt);
-        //?Esitlik durumu
-        if(userSelectImg.alt === pcSelectImg.alt){
-            draw()
+const calculateResult = () => {
+  // console.log(userSelectImg.alt)
+  // console.log(pcSelectImg.alt)
 
-        }
+  //? Esitlik durumu
+  if (userSelectImg.alt === pcRandom) {
+    draw()
+  } else {
+    if (userSelectImg.alt === "rock") {
+      pcRandom === "paper" ? youLost() : youWin()
+    } else if (userSelectImg.alt === "scissor") {
+      pcRandom === "rock" ? youLost() : youWin()
+    } else if (userSelectImg.alt === "paper") {
+      pcRandom === "scissor" ? youLost() : youWin()
     }
+  }
+}
 
+const draw = () => {
+  messagePar.textContent = "Its a draw"
+  scoreCardSection.style.color = YELLOW
+  messagePar.style.backgroundColor = YELLOW
+}
 
+const youLost = () => {
+  messagePar.textContent = "You Lost"
+  scoreCardSection.style.color = RED
+  messagePar.style.backgroundColor = RED
+  pcScoreSpan.textContent++
+}
+
+const youWin = () => {
+  messagePar.textContent = "You Win"
+  scoreCardSection.style.color = GREEN
+  messagePar.style.backgroundColor = GREEN
+  yourScoreSpan.textContent++
+}
