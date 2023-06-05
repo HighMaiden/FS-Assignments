83b0204f2abb71410e1304bac441b587f0500f6e
Ali Ozkan <aliozkan4334@gmail.com>
2023-06-05 20:35:49 +0200

    add function for selectors

--- Assignments/Assignment-24 RPS/app.js
+++ Assignments/Assignment-24 RPS/app.js
@@ -10,25 +10,67 @@ const paperImg=document.getElementById("paper")
 const scissorImg=document.getElementById("scissor")
 //?Secilen Elemanlarin Tasiyicilari
 const yourChoiceDiv= document.getElementById("your-choice")
+const selectionArticle = document.querySelector(".selection")
 
 //* ------- Variables ------- */
-let image = document.createElement("img")
+let userSelectImg = document.createElement("img")
 
 
 
 
 
 //* ------- Event Listeners ------- */
-
-rockImg.addEventListener("click",()=>{
-
-image.src="./assets/rock.png"
-image.alt="rock"
-yourChoiceDiv.appendChild(image)
-
-//?INNERHTML
-// yourChoiceDiv.innerHTML =`<img src="./assets/rock.png" alt="rock"  >`
+selectionArticle.addEventListener("click",(e)=>{
+    // console.log(e.target.id);
+    if(e.target.id){
+    //?can be done like this also
+    // if(
+    // e.target.id==="rock" ||
+    // e.target.id ==="scissor"||
+    // e.target.id === "paper"
+    // ){
+        userSelectImg.src =`./assets/${e.target.id}.png`
+        userSelectImg.alt = e.target.id
+        yourChoiceDiv.appendChild(userSelectImg)
+        createPcSelection()
+    }
 })
+
+
+
+const createPcSelection = ()=>{
+const pcArr=["rock","paper","scissor"]
+const pcRandom=pcArr[Math.floor(Math.random()*3)]
+
+
+}
+
+// rockImg.addEventListener("click",()=>{
+
+// image.src="./assets/rock.png"
+// image.alt="rock"
+// yourChoiceDiv.appendChild(image)
+
+// //?INNERHTML
+// // yourChoiceDiv.innerHTML =`<img src="./assets/rock.png" alt="rock"  >`
+// })
+// paperImg.addEventListener("click",()=>{
+
+//     image.src="./assets/paper.png"
+//     image.alt="paper"
+//     yourChoiceDiv.appendChild(image)
+    
+
+//     })
+//     scissorImg.addEventListener("click",()=>{
+
+//         image.src="./assets/scissor.png"
+//         image.alt="scissor"
+//         yourChoiceDiv.appendChild(image)
+        
+    
+//         })
+    
 
 
 
